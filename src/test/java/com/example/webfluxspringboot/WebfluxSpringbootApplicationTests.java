ce1b36cb5eb9f66a39fa37fec945ff6efc4dd3a6
77kkyu <77kkyu@gmail.com>
2022-05-17 22:47:04 +0900

    setting

---
+++ src/test/java/com/example/webfluxspringboot/WebfluxSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
