2ad874a4b03cc98405087fc4db02dee719c2c7c4
77kkyu <77kkyu@gmail.com>
2022-05-27 22:56:42 +0900

    feat application

--- src/main/java/com/example/webfluxspringboot/WebfluxSpringbootApplication.java
+++ src/main/java/com/example/webfluxspringboot/WebfluxSpringbootApplication.java
@@ -1,13 +1,21 @@
 package com.example.webfluxspringboot;
 
+import com.example.webfluxspringboot.dev.webClient.GreetingWebClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class WebfluxSpringbootApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(WebfluxSpringbootApplication.class, args);
+
+        GreetingWebClient gwc = new GreetingWebClient();
+        log.info(gwc.getResult());
+
     }
 
 }
