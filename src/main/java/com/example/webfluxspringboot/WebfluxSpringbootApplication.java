037d3ecb618bbedb60b2e7520971aa1057ed3b93
77kkyu <77kkyu@gmail.com>
2022-05-30 20:19:09 +0900

    feat application

--- src/main/java/com/example/webfluxspringboot/WebfluxSpringbootApplication.java
+++ src/main/java/com/example/webfluxspringboot/WebfluxSpringbootApplication.java
@@ -4,6 +4,12 @@ import com.example.webfluxspringboot.dev.webClient.GreetingWebClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 
 @Slf4j
 @SpringBootApplication
@@ -15,6 +21,11 @@ public class WebfluxSpringbootApplication {
 
         GreetingWebClient gwc = new GreetingWebClient();
         log.info(gwc.getResult());
+
+//        @Bean
+//        RouterFunction<ServerResponse> routes() {
+//            return RouterFunctions.route(RequestPredicates.GET("/"), request -> ServerResponse.ok().body(Flux.just("Hello", "World!"), String.class));
+//        }
 
     }
 
