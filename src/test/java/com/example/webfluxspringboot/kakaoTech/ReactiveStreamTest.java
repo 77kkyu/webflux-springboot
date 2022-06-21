5c7f466fe9ba19e79b907f7c91ac117e42a39961
77kkyu <77kkyu@gmail.com>
2022-06-21 21:36:40 +0900

    test ReactiveStreamTest

--- src/test/java/com/example/webfluxspringboot/kakaoTech/ReactiveStreamTest.java
+++ src/test/java/com/example/webfluxspringboot/kakaoTech/ReactiveStreamTest.java
@@ -47,4 +47,26 @@ public class ReactiveStreamTest {
         }).subscribe(System.out::println);
     }
 
+    @Test
+    public void subscribeOnTest() {
+        basketFlux.concatMap(basket -> {
+            final Mono<List<String>> distinctFruits = Flux.fromIterable(basket).distinct().collectList().subscribeOn(Schedulers.parallel());
+            final Mono<Map<String, Long>> countFruitsMono = Flux.fromIterable(basket)
+                    .groupBy(fruit -> fruit) // 바구니로 부터 넘어온 과일 기준으로 group을 묶는다.
+                    .concatMap(groupedFlux -> groupedFlux.count()
+                            .map(count -> {
+                                final Map<String, Long> fruitCount = new LinkedHashMap<>();
+                                fruitCount.put(groupedFlux.key(), count);
+                                return fruitCount;
+                            }) // 각 과일별로 개수를 Map으로 리턴
+                    ) // concatMap으로 순서보장
+                    .reduce((accumulatedMap, currentMap) -> new LinkedHashMap<String, Long>() { {
+                        putAll(accumulatedMap);
+                        putAll(currentMap);
+                    }}) // 그동안 누적된 accumulatedMap에 현재 넘어오는 currentMap을 합쳐서 새로운 Map을 만든다. // map끼리 putAll하여 하나의 Map으로 만든다.
+                    .subscribeOn(Schedulers.parallel());
+            return Flux.zip(distinctFruits, countFruitsMono, (distinct, count) -> new FruitInfo(distinct, count));
+        }).subscribe(System.out::println);
+    }
+
 }
