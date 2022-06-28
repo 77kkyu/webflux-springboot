9ea89b20aa53262e97d970bab5ee275cdc677cb2
77kkyu <77kkyu@gmail.com>
2022-06-28 21:18:52 +0900

    test ReactiveStreamTest

--- src/test/java/com/example/webfluxspringboot/kakaoTech/ReactiveStreamTest.java
+++ src/test/java/com/example/webfluxspringboot/kakaoTech/ReactiveStreamTest.java
@@ -171,4 +171,38 @@ public class ReactiveStreamTest {
         );
     }
 
+    @Test
+    public void publishOnTest() {
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        basketFlux.concatMap(basket -> {
+            final Flux<String> source = Flux.fromIterable(basket).log().publish().autoConnect(2);
+            final Mono<List<String>> distinctFruits = source.publishOn(Schedulers.parallel()).distinct().collectList().log();
+            final Mono<Map<String, Long>> countFruitsMono = source.publishOn(Schedulers.parallel())
+                    .groupBy(fruit -> fruit) // 바구니로 부터 넘어온 과일 기준으로 group을 묶는다.
+                    .concatMap(groupedFlux -> groupedFlux.count()
+                            .map(count -> {
+                                final Map<String, Long> fruitCount = new LinkedHashMap<>();
+                                fruitCount.put(groupedFlux.key(), count);
+                                return fruitCount;
+                            }) // 각 과일별로 개수를 Map으로 리턴
+                    ) // concatMap으로 순서보장
+                    .reduce((accumulatedMap, currentMap) -> new LinkedHashMap<String, Long>() { {
+                        putAll(accumulatedMap);
+                        putAll(currentMap);
+                    }}) // 그동안 누적된 accumulatedMap에 현재 넘어오는 currentMap을 합쳐서 새로운 Map을 만든다. // map끼리 putAll하여 하나의 Map으로 만든다.
+                    .log();
+            return Flux.zip(distinctFruits, countFruitsMono, (distinct, count) -> new FruitInfo(distinct, count));
+        }).subscribe(
+                System.out::println,  // 값이 넘어올 때 호출 됨, onNext(T)
+                error -> {
+                    System.err.println(error);
+                    countDownLatch.countDown();
+                }, // 에러 발생시 출력하고 countDown, onError(Throwable)
+                () -> {
+                    System.out.println("complete");
+                    countDownLatch.countDown();
+                } // 정상적 종료시 countDown, onComplete()
+        );
+    }
+
 }
