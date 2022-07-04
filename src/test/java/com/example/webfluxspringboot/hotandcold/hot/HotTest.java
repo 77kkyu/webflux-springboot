b1309697b1141270ac3d82bcf28fb8c88e9d65af
77kkyu <77kkyu@gmail.com>
2022-07-04 21:58:20 +0900

    test HotTest

--- src/test/java/com/example/webfluxspringboot/hotandcold/hot/HotTest.java
+++ src/test/java/com/example/webfluxspringboot/hotandcold/hot/HotTest.java
@@ -30,6 +30,45 @@ public class HotTest {
         Thread.sleep(5000L);
     }
 
+    /**
+     * 만약 철규가 모든 영화를 다 보고 동팔이가 영화관에 도착하면 처음부터 다시 보여줌
+     */
+    @Test
+    public void cinema2() throws InterruptedException {
+        final Flux<String> hotFlux = Flux.fromStream(this::getMovies)
+                .doOnSubscribe(x -> System.out.println("스트리밍 시작!"))
+                .delayElements(Duration.ofSeconds(1))
+                .share();
+
+        hotFlux.publishOn(Schedulers.newBoundedElastic(1, 1, "철규"))
+                .subscribe(scene -> System.out.println("철규가 보는 영화 " + scene));
+        Thread.sleep(6000L);
+
+        hotFlux.publishOn(Schedulers.newBoundedElastic(1, 1, "동팔"))
+                .subscribe(scene -> System.out.println("동팔이 보는 영화 " + scene));
+        Thread.sleep(5000L);
+    }
+
+    /**
+     *  publish().autoConnect()를 사용하면 모든 영화가 끝난 이후에 도착하면 동작 안함
+     */
+    @Test
+    public void cinema_autoConnect() throws InterruptedException {
+        final Flux<String> hotFlux = Flux.fromStream(this::getMovies)
+                .doOnSubscribe(x -> System.out.println("스트리밍 시작!"))
+                .delayElements(Duration.ofSeconds(1))
+                .publish()
+                .autoConnect();
+
+        hotFlux.publishOn(Schedulers.newBoundedElastic(1, 1, "철규"))
+                .subscribe(scene -> System.out.println("철규가 보는 영화 " + scene));
+        Thread.sleep(6000L);
+
+        hotFlux.publishOn(Schedulers.newBoundedElastic(1, 1, "동팔"))
+                .subscribe(scene -> System.out.println("동팔이 보는 영화 " + scene));
+        Thread.sleep(5000L);
+    }
+
     private Stream<String> getMovies() {
         return Stream.of("범죄도시2", "타짜", "독전", "아이언맨");
     }
