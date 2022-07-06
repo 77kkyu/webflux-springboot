614a5b3d43a0dfd87867427940cb11998fec326b
77kkyu <77kkyu@gmail.com>
2022-07-06 22:12:04 +0900

    test HotTest

--- src/test/java/com/example/webfluxspringboot/hotandcold/hot/HotTest.java
+++ src/test/java/com/example/webfluxspringboot/hotandcold/hot/HotTest.java
@@ -69,6 +69,26 @@ public class HotTest {
         Thread.sleep(5000L);
     }
 
+    /**
+     * cache()를 사용하면 늦께 와도 마치 녹화한 영상을 보여주는 것과 똑같은 원리
+     * 스트리밍은 한 번 했지만 계속 볼 수 있다
+     */
+    @Test
+    public void cinema_cache() throws InterruptedException {
+        final Flux<String> hotFlux = Flux.fromStream(this::getMovies)
+                .doOnSubscribe(x -> System.out.println("스트리밍 시작!"))
+                .delayElements(Duration.ofSeconds(1))
+                .cache();
+
+        hotFlux.publishOn(Schedulers.newBoundedElastic(1, 1, "철규"))
+                .subscribe(scene -> System.out.println("철규가 보는 영화 " + scene));
+        Thread.sleep(6000L);
+
+        hotFlux.publishOn(Schedulers.newBoundedElastic(1, 1, "동팔"))
+                .subscribe(scene -> System.out.println("동팔이 보는 영화 " + scene));
+        Thread.sleep(5000L);
+    }
+
     private Stream<String> getMovies() {
         return Stream.of("범죄도시2", "타짜", "독전", "아이언맨");
     }
