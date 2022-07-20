7aa37b9660ee05c73ba701f373dd132d45d0fcab
77kkyu <77kkyu@gmail.com>
2022-07-20 21:10:32 +0900

    fix BoardRepository

--- src/main/java/com/example/webfluxspringboot/dev/repository/BoardRepository.java
+++ src/main/java/com/example/webfluxspringboot/dev/repository/BoardRepository.java
@@ -1,7 +1,8 @@
 package com.example.webfluxspringboot.dev.repository;
 
-import com.example.webfluxspringboot.dev.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+import com.example.webfluxspringboot.dev.domain.Board;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface BoardRepository extends ReactiveCrudRepository<Board, Long> {
 }
