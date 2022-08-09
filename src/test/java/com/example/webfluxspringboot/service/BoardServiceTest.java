2bb8d697cf883018bb3eae0d8602d0488164a37b
77kkyu <77kkyu@gmail.com>
2022-08-09 22:12:17 +0900

    test BoardServiceTest

--- src/test/java/com/example/webfluxspringboot/service/BoardServiceTest.java
+++ src/test/java/com/example/webfluxspringboot/service/BoardServiceTest.java
@@ -1,10 +1,13 @@
 package com.example.webfluxspringboot.service;
 
+import com.example.webfluxspringboot.dev.domain.Board;
 import com.example.webfluxspringboot.dev.dto.BoardAddResponseDto;
+import com.example.webfluxspringboot.dev.dto.BoardsResponseDto;
 import com.example.webfluxspringboot.dev.service.BoardService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 @SpringBootTest
 public class BoardServiceTest {
@@ -21,6 +24,11 @@ public class BoardServiceTest {
     @Test
     public void getBoard() {
         boardService.getBoard().subscribe();
+    }
+
+    @Test
+    public void getBoards() {
+        boardService.boards().subscribe(System.out::println);
     }
 
 }
