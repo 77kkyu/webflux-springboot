c1242796771ad5c95275c16bfeb672bd65e0825c
77kkyu <77kkyu@gmail.com>
2022-08-16 23:29:40 +0900

    feat

--- src/main/java/com/example/webfluxspringboot/dev/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.webfluxspringboot.dev.repository;
-
-
-import com.example.webfluxspringboot.dev.domain.Board;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface BoardRepository extends ReactiveCrudRepository<Board, Long> {
-}
