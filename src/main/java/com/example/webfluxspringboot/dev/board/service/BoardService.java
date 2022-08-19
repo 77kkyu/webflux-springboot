7abfa7bd508835b4926388a4157bdfa0529ac0fd
77kkyu <77kkyu@gmail.com>
2022-08-19 23:18:56 +0900

    feat

---
+++ src/main/java/com/example/webfluxspringboot/dev/board/service/BoardService.java
@@ -0,0 +1,62 @@
+package com.example.webfluxspringboot.dev.board.service;
+
+import com.example.webfluxspringboot.dev.board.domain.Board;
+import com.example.webfluxspringboot.dev.board.dto.request.BoardAddRequestDto;
+import com.example.webfluxspringboot.dev.board.dto.response.BoardAddResponseDto;
+import com.example.webfluxspringboot.dev.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public void boardAdd(BoardAddResponseDto boardAddResponseDto) {
+        boardRepository.save(Board.builder()
+            .title(boardAddResponseDto.getTitle())
+            .contents(boardAddResponseDto.getContents())
+            .userId(boardAddResponseDto.getUserId())
+            .build()
+        ).subscribe();
+    }
+
+    public Flux<Board> getBoard() {
+        return boardRepository.findAll();
+    }
+
+    public Mono<BoardAddResponseDto> boardAdd(BoardAddRequestDto boardAddRequestDto) {
+        return boardRepository.save(Board.builder()
+            .title(boardAddRequestDto.getTitle())
+            .contents(boardAddRequestDto.getContents())
+            .userId(boardAddRequestDto.getUserId())
+            .build()
+        ).map(board -> {
+            return BoardAddResponseDto.builder()
+                .title(board.getTitle())
+                .contents(board.getContents())
+                .userId(board.getUserId())
+                .build();
+        }).doOnError(RuntimeException -> {
+            throw new RuntimeException("board add error");
+        });
+
+    }
+
+    public Flux boardNewTitle() {
+        Flux flux = (Flux) boardRepository.findAll()
+                .filter(e -> e.getTitle() == null)
+                .map(x -> x.getTitle() == "새 제목")
+                .subscribe();
+        return flux;
+    }
+
+    public Mono findBoard(Long id) {
+        return boardRepository.findById(id);
+    }
+
+}
