a2214f70845e53849fbf539bdf07d03feb0c8b7e
77kkyu <77kkyu@gmail.com>
2022-08-23 23:02:56 +0900

    feat

---
+++ src/main/java/com/example/webfluxspringboot/dev/board/controller/BoardController.java
@@ -0,0 +1,24 @@
+package com.example.webfluxspringboot.dev.board.controller;
+
+import com.example.webfluxspringboot.dev.board.dto.request.BoardAddRequestDto;
+import com.example.webfluxspringboot.dev.board.dto.response.BoardAddResponseDto;
+import com.example.webfluxspringboot.dev.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@Log4j2
+@RequiredArgsConstructor
+@RestController
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping("/board/add")
+    public Mono<BoardAddResponseDto> boardAdd(BoardAddRequestDto boardAddRequestDto) {
+        return boardService.boardAdd(boardAddRequestDto);
+    }
+
+}
