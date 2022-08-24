31924ea19a65bc8e1e162ce6e3887cbbf74b33be
77kkyu <77kkyu@gmail.com>
2022-08-24 22:57:43 +0900

    feat

--- src/main/java/com/example/webfluxspringboot/dev/board/controller/BoardController.java
+++ src/main/java/com/example/webfluxspringboot/dev/board/controller/BoardController.java
@@ -2,11 +2,14 @@ package com.example.webfluxspringboot.dev.board.controller;
 
 import com.example.webfluxspringboot.dev.board.dto.request.BoardAddRequestDto;
 import com.example.webfluxspringboot.dev.board.dto.response.BoardAddResponseDto;
+import com.example.webfluxspringboot.dev.board.dto.response.BoardsResponseDto;
 import com.example.webfluxspringboot.dev.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Log4j2
@@ -19,6 +22,11 @@ public class BoardController {
     @PostMapping("/board/add")
     public Mono<BoardAddResponseDto> boardAdd(BoardAddRequestDto boardAddRequestDto) {
         return boardService.boardAdd(boardAddRequestDto);
+    }
+
+    @GetMapping("/board/{id}")
+    public Mono<BoardsResponseDto> boards(Long id) {
+        return boardService.findBoard(id);
     }
 
 }
